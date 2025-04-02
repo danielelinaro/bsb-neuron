fix: fixed computation of extracellular voltage

bsb_neuron/devices/efield_stim.py
@@ -9,12 +9,28 @@ from bsb_neuron.device import NeuronDevice
 def compute_Ve_over_cell(
     cell, Efield=None, Emag=None, theta=None, phi=None, Ve_soma=0, full_output=False
 ):
+    from neuron import nrn
+
+    if hasattr(cell, "sections"):
+        # potentially a cell from the repo of the Brain and Behavioral
+        # Sciences Laboratory at the University of Pavia
+        cell_secs = cell.sections
+    elif isinstance(cell.soma[0], nrn.Section) and hasattr(cell, "all"):
+        # potentially a cell from the repo of the Blue Brain Project
+        cell_secs = [sec for sec in cell.all]
+    else:
+        raise Exception("Do not know how to deal with this type of cell")
+
     if Efield is not None:
         const_E = False
     elif Emag is not None and theta is not None and phi is not None:
         const_E = True
+        sin_th, cos_th = np.sin(theta), np.cos(theta)
+        sin_ph, cos_ph = np.sin(phi), np.cos(phi)
     else:
         raise Exception("Either Efield or Emag, theta and phi must be passed")
+
+    from scipy.interpolate import interp1d
 
     # potential difference between the parent and the present points
     _compute_Ve = lambda x_c, x_p, Ve_p, Efun: Ve_p - 0.5 * (Efun(x_c) + Efun(x_p)) @ (
@@ -31,60 +47,68 @@ def compute_Ve_over_cell(
                     sec.y3d(i),  # y coord
                     sec.z3d(i),  # z coord
                     sec.arc3d(i) / sec.L,  # relative position in section
-                    Ve_soma,  # electrical potential (will be filled later)
+                    # electrical potential (will be filled later)
+                    Ve_soma,
                 ]
                 for i in range(sec.n3d())
             ]
         )
-        for sec in cell.sections
+        for sec in cell_secs
     }
-    segments = {sec.name(): np.zeros(sec.nseg) + Ve_soma for sec in cell.sections}
+    segments = {sec.name(): np.zeros(sec.nseg) + Ve_soma for sec in cell_secs}
 
-    for sec in cell.sections:
+    for sec in cell_secs:
         key_c = sec.name()
         if const_E:
             for j in range(sec.n3d()):
                 x, y, z = points_data[key_c][j][:3] * 1e-6  # [m]
-                points_data[key_c][j][-1] = -Emag * (
-                    x * np.sin(theta) * np.cos(phi)
-                    + y * np.sin(theta) * np.sin(phi)
-                    + z * np.cos(theta)
+                points_data[key_c][j, -1] = -Emag * (
+                    x * sin_th * cos_ph + y * sin_th * sin_ph + z * cos_th
                 )
         else:
             if sec.parentseg() is not None:
-                X_c = points_data[key_c][0][:3]
+                X_c = points_data[key_c][0, :3]
                 key_p = sec.parentseg().sec.name()
-                if np.sum((X_c - points_data[key_p][-1][:3]) ** 2) < 1e-6:
-                    Ve = points_data[key_p][-1][-1]
+                if np.sum((X_c - points_data[key_p][-1, :3]) ** 2) < 1e-6:
+                    Ve = points_data[key_p][-1, -1]
                 else:
-                    X_p = points_data[key_p][-2][:3]
+                    X_p = points_data[key_p][-2, :3]
                     assert np.sum((X_c - X_p) ** 2) > 0
-                    Ve = _compute_Ve(X_c, X_p, points_data[key_p][-2][-1], Efield)
-                points_data[key_c][0][-1] = Ve
+                    Ve = _compute_Ve(X_c, X_p, points_data[key_p][-2, -1], Efield)
+                points_data[key_c][0, -1] = Ve
             for j in range(1, sec.n3d()):
-                X_c = points_data[key_c][j][:3]
-                X_p = points_data[key_c][j - 1][:3]
-                points_data[key_c][j][-1] = _compute_Ve(
-                    X_c, X_p, points_data[key_c][j - 1][-1], Efield
+                X_c = points_data[key_c][j, :3]
+                X_p = points_data[key_c][j - 1, :3]
+                points_data[key_c][j, -1] = _compute_Ve(
+                    X_c, X_p, points_data[key_c][j - 1, -1], Efield
                 )
 
-        bins = np.linspace(0, 1, sec.nseg + 1)
-        pos = points_data[key_c][:, 3]
-        idx = np.digitize(pos, bins, right=False) - 1
-        idx[idx == sec.nseg] = sec.nseg - 1
-        for j in range(sec.nseg):
-            (jdx,) = np.where(idx == j)
-            if jdx.size == 0:
-                if j == 0:
-                    Ve = segments[key_p][-1]
+        if sec.n3d() == 1:
+            segments[key_c][0] = points_data[key_c][0, -1]
+        else:
+            bins = np.linspace(0, 1, sec.nseg + 1)
+            pos = points_data[key_c][:, 3]
+            idx = np.digitize(pos, bins, right=False) - 1
+            idx[idx == sec.nseg] = sec.nseg - 1
+            for j, seg in enumerate(sec):
+                (jdx,) = np.where(idx == j)
+                if jdx.size == 0:
+                    if j == 0:
+                        Ve = segments[key_p][-1]
+                    else:
+                        Ve = segments[key_c][j - 1]
                 else:
-                    Ve = segments[key_c][j - 1]
-            else:
-                Ve = points_data[key_c][jdx, -1].mean()
-            segments[key_c][j] = Ve
-    V_extra = [segments[sec.name()] for sec in cell.sections]
+                    if seg.x < points_data[key_c][jdx[0], 3]:
+                        jdx = np.concatenate(([jdx[0] - 1], jdx))
+                    if seg.x > points_data[key_c][jdx[-1], 3]:
+                        jdx = np.concatenate((jdx, [jdx[-1] + 1]))
+                    Ve = interp1d(points_data[key_c][jdx, 3], points_data[key_c][jdx, 4])(
+                        seg.x
+                    )
+                segments[key_c][j] = Ve
+    V_extra = [segments[sec.name()] for sec in cell_secs]
     if full_output:
-        return V_extra, cell.sections, points_data
+        return V_extra, cell_secs, points_data
     return V_extra
 
 